653e4863a229847ea9ed73ae185fb72d8de67285
Pantakan Rungwannarat <pantakan-pp21@hotmail.com>
2025-12-25 01:10:05 +0700

    ปรับ Wording

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -25,7 +25,7 @@
       },
       {
         icon: "code",
-        role: "Web Master Trainee",
+        role: "Web Master Support Trainee",
         period: "2023",
         company: "Tero Entertainment",
         summary:
